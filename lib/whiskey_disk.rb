0ca21a240c9b07e65407261f5f00d5a1c2d0f4c0
Rick Bradley <rick@rickbradley.com>
2009-12-31 22:14:34 +0530

    this is one too many requires.  weird action-at-a-distance behavior :-/

--- lib/whiskey_disk.rb
+++ lib/whiskey_disk.rb
@@ -1,4 +1,3 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'tasks', 'deploy'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'whiskey_disk', 'config'))
 
 class WhiskeyDisk
