ebaa18f58991b6ed5e63e6edec2832541a85ed84
Rick Bradley <rick@rickbradley.com>
2010-05-02 23:03:12 -0500

    Removing the "features" of having per-environment configuration files.
    I am going to make the overall config file handling much more sophisticated in
    the next feature release, so this is just a distraction.

--- lib/whiskey_disk/config.rb
+++ lib/whiskey_disk/config.rb
@@ -19,24 +19,13 @@ class WhiskeyDisk
         Dir.pwd
       end
       
-      def main_configuration_file
+      def configuration_file
         File.expand_path(File.join(base_path, 'config', 'deploy.yml'))
       end
       
-      def main_configuration_data
-        raise "Main configuration file [#{main_configuration_file}] not found!" unless File.exists?(main_configuration_file)
-        File.read(main_configuration_file)
-      end
-      
-      def environment_configuration_file
-        raise "Cannot determine current environment -- try rake ... to=staging, for example." unless environment_name
-        File.expand_path(File.join(base_path, 'config', "deploy-#{environment_name}.yml"))
-      end
-      
-      def environment_configuration_data
-        File.exists?(environment_configuration_file) ? File.read(environment_configuration_file) : nil
-      rescue Exception => e
-        raise %Q{Could not read configuration file [#{environment_configuration_file}] for environment [#{environment_name}]: "#{e}"}
+      def configuration_data
+        raise "Configuration file [#{configuration_file}] not found!" unless File.exists?(configuration_file)
+        File.read(configuration_file)
       end
       
       def project_name(config)
@@ -44,33 +33,23 @@ class WhiskeyDisk
         config['repository'].sub(%r{^.*[/:]}, '').sub(%r{\.git$}, '')
       end
       
-      def load_main_data
-        YAML.load(main_configuration_data)
+      def load_data
+        YAML.load(configuration_data)
       rescue Exception => e
-        raise %Q{Error reading configuration file [#{main_configuration_file}]: "#{e}"}
-      end
-      
-      def load_environment_data        
-        begin
-          env = environment_configuration_data ? YAML.load(environment_configuration_data) : nil
-        rescue Exception => e
-          raise %Q{Error reading configuration file [#{environment_configuration_file}]: "#{e}"}
-        end
-        raise "Configuration file [#{environment_configuration_file}] does not define data for environment [#{environment_name}]" if env and !env[environment_name]
-        env || {}
+        raise %Q{Error reading configuration file [#{configuration_file}]: "#{e}"}
       end
       
       def fetch
         raise "Cannot determine current environment -- try rake ... to=staging, for example." unless environment_name
-        main, env = load_main_data, load_environment_data
-        raise "No configuration file defined data for environment [#{environment_name}]" unless main[environment_name] or env[environment_name]
-        config = (main[environment_name] || {}).merge(env[environment_name] || {}).merge({'environment' => environment_name})
+        data = load_data
+        raise "No configuration file defined data for environment [#{environment_name}]" unless data[environment_name]
+        config = (data[environment_name] || {}).merge({'environment' => environment_name})
         { 'project' => project_name(config) }.merge(config)
       end
     
       def filenames
         raise "Cannot determine current environment -- try rake ... to=staging, for example." unless environment_name
-        [ main_configuration_file, environment_configuration_file ]
+        [ configuration_file ]
       end
     end
   end
--- spec/whiskey_disk/config_spec.rb
+++ spec/whiskey_disk/config_spec.rb
@@ -6,205 +6,81 @@ CURRENT = File.expand_path(File.dirname(__FILE__))  # BACON evidently mucks arou
 
 describe WhiskeyDisk::Config do
   describe 'when fetching configuration' do
+    before do
+      ENV['to'] = @env = 'staging'
+    end
+    
     it 'should fail if the current environment cannot be determined' do
       ENV['to'] = nil
       lambda { WhiskeyDisk::Config.fetch }.should.raise
     end
     
-    describe 'when there is no separate configuration file for the current environment' do
-      before do
-        ENV['to'] = @env = 'staging'
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(nil)
-      end
-      
-      it 'should fail if the main configuration file does not exist' do
-        WhiskeyDisk::Config.stub!(:main_configuration_file).and_return(__FILE__ + "_.crap")
-        lambda { WhiskeyDisk::Config.fetch }.should.raise
-      end
-      
-      it 'should fail if the main configuration file cannot be read' do
-        WhiskeyDisk::Config.stub!(:main_configuration_file).and_return("/tmp")
-        lambda { WhiskeyDisk::Config.fetch }.should.raise        
-      end
-      
-      it 'should fail if the main configuration file is invalid' do
-        YAML.stub!(:load).and_raise
-        lambda { WhiskeyDisk::Config.fetch }.should.raise        
-      end
-      
-      it 'should fail if the main configuration file does not define data for this environment' do
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'} }))
-        lambda { WhiskeyDisk::Config.fetch }.should.raise
-      end
-      
-      it 'should return the main configuration yaml file data for this environment as a hash' do
-        staging = { 'foo' => 'bar', 'baz' => 'xyzzy' }
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
-        result = WhiskeyDisk::Config.fetch
-        staging.each_pair do |k,v|
-          result[k].should == v
-        end
-      end
-      
-      it 'should not include configuration information for other environments in the returned hash' do
-        staging = { 'foo' => 'bar', 'baz' => 'xyzzy' }
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
-        WhiskeyDisk::Config.fetch['a'].should.be.nil
-      end
-      
-      it 'should include the environment in the hash' do
-        staging = { 'foo' => 'bar', 'baz' => 'xyzzy' }
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
-        WhiskeyDisk::Config.fetch['environment'].should == 'staging'
-      end
-      
-      it 'should not allow overriding the environment in the configuration file' do
-        staging = { 'foo' => 'bar', 'baz' => 'xyzzy', 'environment' => 'production' }
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
-        WhiskeyDisk::Config.fetch['environment'].should == 'staging'
-      end
-      
-      it 'should include the project handle in the hash' do
-        staging = { 'foo' => 'bar', 'baz' => 'xyzzy' }
-        WhiskeyDisk::Config.stub!(:project_name).and_return('whiskey_disk')
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
-        WhiskeyDisk::Config.fetch['project'].should == 'whiskey_disk'
-      end
-      
-      it 'should allow overriding the project handle in the configuration file' do
-        staging = { 'foo' => 'bar', 'baz' => 'xyzzy', 'project' => 'diskey_whisk' }
-        WhiskeyDisk::Config.stub!(:project_name).and_return('whiskey_disk')
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
-        WhiskeyDisk::Config.fetch['project'].should == 'diskey_whisk'
+    it 'should fail if the configuration file does not exist' do
+      WhiskeyDisk::Config.stub!(:configuration_file).and_return(__FILE__ + "_.crap")
+      lambda { WhiskeyDisk::Config.fetch }.should.raise
+    end
+    
+    it 'should fail if the configuration file cannot be read' do
+      WhiskeyDisk::Config.stub!(:configuration_file).and_return("/tmp")
+      lambda { WhiskeyDisk::Config.fetch }.should.raise        
+    end
+    
+    it 'should fail if the configuration file is invalid' do
+      YAML.stub!(:load).and_raise
+      lambda { WhiskeyDisk::Config.fetch }.should.raise        
+    end
+    
+    it 'should fail if the configuration file does not define data for this environment' do
+      WhiskeyDisk::Config.stub!(:configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'} }))
+      lambda { WhiskeyDisk::Config.fetch }.should.raise
+    end
+    
+    it 'should return the configuration yaml file data for this environment as a hash' do
+      staging = { 'foo' => 'bar', 'baz' => 'xyzzy' }
+      WhiskeyDisk::Config.stub!(:configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
+      result = WhiskeyDisk::Config.fetch
+      staging.each_pair do |k,v|
+        result[k].should == v
       end
     end
-
-    describe 'when there is a separate configuration file for the current environment' do
-      before do
-        ENV['to'] = @env = 'staging'
-        WhiskeyDisk::Config.stub!(:environment_configuration_file).and_return(__FILE__)
-        WhiskeyDisk::Config.stub!(:main_configuration_file).and_return(__FILE__)
-      end
-      
-      it 'should fail if the main configuration file does not exist' do
-        WhiskeyDisk::Config.stub!(:main_configuration_file).and_return(__FILE__ + "_.crap")
-        lambda { WhiskeyDisk::Config.fetch }.should.raise(RuntimeError)
-      end
-      
-      it 'should fail if the main configuration file cannot be read' do
-        WhiskeyDisk::Config.stub!(:main_configuration_file).and_return("/tmp")
-        lambda { WhiskeyDisk::Config.fetch }.should.raise(RuntimeError)    
-      end
-      
-      it 'should fail if the main configuration file is invalid' do
-        YAML.stub!(:load).and_raise
-        lambda { WhiskeyDisk::Config.fetch }.should.raise        
-      end
-      
-      it 'should fail if the separate configuration file cannot be read' do
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({}))
-        WhiskeyDisk::Config.stub!(:environment_configuration_file).and_return("/tmp")
-        lambda { WhiskeyDisk::Config.fetch }.should.raise        
-      end
-      
-      it 'should fail if the separate configuration file is invalid' do
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({}))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return('BAD DATA')
-        YAML.stub!(:load).with('BAD DATA').and_raise
-        lambda { WhiskeyDisk::Config.fetch }.should.raise        
-      end
-      
-      it 'should fail if the separate configuration file does not define data for this environment' do
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump({}))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'} }))
-        lambda { WhiskeyDisk::Config.fetch }.should.raise
-      end
-      
-      it 'should return the merger of main and separate yaml configuration data as a hash' do
-        main = { 'staging' => { 'foo' => 'bar'}}
-        env  = { 'staging' => { 'baz' => 'xyzzy'}}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        result = WhiskeyDisk::Config.fetch
-        main.merge(env)['staging'].each_pair {|k,v| result[k].should == v}
-      end
-      
-      it 'should work even if main does not provide configuration data for this environment' do
-        main = { 'production' => { 'foo' => 'bar'}}
-        env  = { 'staging' => { 'baz' => 'xyzzy'}}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        result = WhiskeyDisk::Config.fetch
-        env['staging'].each_pair {|k,v| result[k].should == v}
-      end
-      
-      it 'should override main configuration file data with separate configuration file data when there is a conflict' do
-        main = { 'production' => { 'foo' => 'bar'}}
-        env  = { 'staging' => { 'baz' => 'xyzzy', 'foo' => 'mine'}}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        result = WhiskeyDisk::Config.fetch
-        env['staging'].each_pair {|k,v| result[k].should == v}        
-      end
-      
-      it 'should include the environment in the hash' do
-        main = { 'staging' => { 'foo' => 'bar'}}
-        env  = { 'staging' => { 'baz' => 'xyzzy'}}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        WhiskeyDisk::Config.fetch['environment'].should == 'staging'
-      end
-      
-      it 'should not allow overriding the environment in the main configuration file' do
-        main = { 'staging' => { 'foo' => 'bar', 'environment' => 'production'}}
-        env  = { 'staging' => { 'baz' => 'xyzzy'}}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        WhiskeyDisk::Config.fetch['environment'].should == 'staging'        
-      end
-      
-      it 'should not allow overriding the environment in the separate configuration file' do
-        main = { 'staging' => { 'foo' => 'bar', 'environment' => 'production'}}
-        env  = { 'staging' => { 'baz' => 'xyzzy', 'environment' => 'production'}}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        WhiskeyDisk::Config.fetch['environment'].should == 'staging'        
-      end
-      
-      it 'should include the project handle in the hash' do
-        main = { 'staging' => { 'foo' => 'bar' }}
-        env  = { 'staging' => { 'baz' => 'xyzzy' }}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        WhiskeyDisk::Config.stub!(:project_name).and_return('whiskey_disk')
-        WhiskeyDisk::Config.fetch['project'].should == 'whiskey_disk'
-      end
-      
-      it 'should allow overriding the project handle in the main configuration file' do
-        main = { 'staging' => { 'foo' => 'bar', 'project' => 'diskey_whisk' }}
-        env  = { 'staging' => { 'baz' => 'xyzzy' }}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        WhiskeyDisk::Config.stub!(:project_name).and_return('whiskey_disk')
-        WhiskeyDisk::Config.fetch['project'].should == 'diskey_whisk'        
-      end
-      
-      it 'should allow overriding the project handle in the separate configuration file' do
-        main = { 'staging' => { 'foo' => 'bar', 'project' => 'diskey_whisk' }}
-        env  = { 'staging' => { 'baz' => 'xyzzy', 'project' => 'diskey_whisk' }}
-        WhiskeyDisk::Config.stub!(:main_configuration_data).and_return(YAML.dump(main))
-        WhiskeyDisk::Config.stub!(:environment_configuration_data).and_return(YAML.dump(env))
-        WhiskeyDisk::Config.stub!(:project_name).and_return('whiskey_disk')
-        WhiskeyDisk::Config.fetch['project'].should == 'diskey_whisk'        
-      end
-    end    
+    
+    it 'should not include configuration information for other environments in the returned hash' do
+      staging = { 'foo' => 'bar', 'baz' => 'xyzzy' }
+      WhiskeyDisk::Config.stub!(:configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
+      WhiskeyDisk::Config.fetch['a'].should.be.nil
+    end
+    
+    it 'should include the environment in the hash' do
+      staging = { 'foo' => 'bar', 'baz' => 'xyzzy' }
+      WhiskeyDisk::Config.stub!(:configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
+      WhiskeyDisk::Config.fetch['environment'].should == 'staging'
+    end
+    
+    it 'should not allow overriding the environment in the configuration file' do
+      staging = { 'foo' => 'bar', 'baz' => 'xyzzy', 'environment' => 'production' }
+      WhiskeyDisk::Config.stub!(:configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
+      WhiskeyDisk::Config.fetch['environment'].should == 'staging'
+    end
+    
+    it 'should include the project handle in the hash' do
+      staging = { 'foo' => 'bar', 'baz' => 'xyzzy' }
+      WhiskeyDisk::Config.stub!(:project_name).and_return('whiskey_disk')
+      WhiskeyDisk::Config.stub!(:configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
+      WhiskeyDisk::Config.fetch['project'].should == 'whiskey_disk'
+    end
+    
+    it 'should allow overriding the project handle in the configuration file' do
+      staging = { 'foo' => 'bar', 'baz' => 'xyzzy', 'project' => 'diskey_whisk' }
+      WhiskeyDisk::Config.stub!(:project_name).and_return('whiskey_disk')
+      WhiskeyDisk::Config.stub!(:configuration_data).and_return(YAML.dump({ 'production' => { 'a' => 'b'}, 'staging' => staging }))
+      WhiskeyDisk::Config.fetch['project'].should == 'diskey_whisk'
+    end
   end
   
   describe 'when returning the configuration filenames' do    
     before do
       ENV['to'] = @env = 'staging'
-      WhiskeyDisk::Config.stub!(:main_configuration_file).and_return('/path/to/main')
-      WhiskeyDisk::Config.stub!(:environment_configuration_file).and_return('/path/to/staging')
+      WhiskeyDisk::Config.stub!(:configuration_file).and_return('/path/to/config')
     end
 
     it 'should fail if the current environment cannot be determined' do
@@ -212,12 +88,8 @@ describe WhiskeyDisk::Config do
       lambda { WhiskeyDisk::Config.filenames }.should.raise
     end
     
-    it 'should include the location of the main configuration file' do
-      WhiskeyDisk::Config.filenames.should.include('/path/to/main')
-    end
-
-    it 'should include the location of a separate configuration file for this environment' do
-      WhiskeyDisk::Config.filenames.should.include('/path/to/staging')
+    it 'should include the location of the configuration file' do
+      WhiskeyDisk::Config.filenames.should.include('/path/to/config')
     end
   end
 
@@ -250,20 +122,7 @@ describe WhiskeyDisk::Config do
   describe 'finding the main configuration file' do
     it 'should return the path to deploy.yml in the config directory under the project base path' do
       WhiskeyDisk::Config.stub!(:base_path).and_return('/path/to/project')
-      WhiskeyDisk::Config.main_configuration_file.should == '/path/to/project/config/deploy.yml'
-    end
-  end
-
-  describe 'finding the per-environment configuration file' do
-    it 'should fail if the current environment cannot be determined' do
-      ENV['to'] = nil
-      lambda { WhiskeyDisk::Config.environment_configuration_file }.should.raise
-    end
-    
-    it 'should return the path to deploy-<environment>.yml in the config directory under the project base path' do
-      ENV['to'] = @env = 'staging'
-      WhiskeyDisk::Config.stub!(:base_path).and_return('/path/to/project')
-      WhiskeyDisk::Config.environment_configuration_file.should == '/path/to/project/config/deploy-staging.yml'
+      WhiskeyDisk::Config.configuration_file.should == '/path/to/project/config/deploy.yml'
     end
   end
   
