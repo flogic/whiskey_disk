afbef35e2de4b4b3333e997d056132ac47e5eedf
Rick Bradley <rick@rickbradley.com>
2010-06-13 21:10:55 -0500

    first real pass at bin/wd, supporting deploy,setup --to

--- spec/wd_command_spec.rb
+++ spec/wd_command_spec.rb
@@ -6,36 +6,217 @@ def run_command
 end
 
 describe 'wd command' do
-  before do
-    @usage = 'USAGE MESSAGE'
-    self.stub!(:usage).and_return(@usage)
-    self.stub!(:exit)
-    self.stub!(:puts)
-  end
-
   describe 'when no command-line arguments are specified' do
     before do
       Object.send(:remove_const, :ARGV)
       ARGV = []
     end  
-
-    it 'should display a usage message' do
-      self.should.receive(:puts).with(@usage)
-      run_command
-    end
     
     it 'should not run rake tasks' do
       Rake::Application.should.receive(:new).never
-      run_command
+      lambda { run_command }
     end
   
-    it 'should exit successfully' do
-      self.should.receive(:exit).with(0)
-      run_command
+    it 'should fail' do
+      lambda { run_command }.should.raise
     end
   end
   
-  describe "when -a is specified on the command-line" do
+  describe "when the 'setup' command is specified" do
+    before do
+      Object.send(:remove_const, :ARGV)
+      ARGV = ['setup']
+    end  
+
+    describe 'and no target is specified' do    
+      it 'should not run rake tasks' do
+        Rake::Application.should.receive(:new).never
+        lambda { run_command }
+      end
+  
+      it 'should fail' do
+        lambda { run_command }.should.raise
+      end
+    end
     
+    describe 'and a --to argument is specified' do
+      before do
+        ARGV.push '--to=foo'
+        @rake = Rake::Task['deploy:setup']
+        @rake.stub!(:invoke)
+      end
+      
+      it 'should not fail' do
+        lambda { run_command }.should.not.raise
+      end
+      
+      it 'should run the deploy:setup rake task' do
+        @rake.should.receive(:invoke)
+        run_command
+      end
+      
+      it 'should make the specified target available as a "to" argument to the rake task' do
+        run_command
+        ENV['to'].should == 'foo'
+      end
+      
+      it 'should fail if the rake task fails' do
+        @rake.stub!(:invoke).and_raise(RuntimeError)
+        lambda { run_command }.should.raise
+      end
+      
+      it 'should not fail if the rake task succeeds' do
+        @rake.stub!(:invoke).and_return(true)
+        lambda { run_command }.should.not.raise
+      end
+    end
+
+    describe 'and a -t argument is specified' do
+      before do
+        ARGV.push '-t'
+        ARGV.push 'foo'
+        @rake = Rake::Task['deploy:setup']
+        @rake.stub!(:invoke)
+      end
+
+      it 'should not fail' do
+        lambda { run_command }.should.not.raise
+      end
+
+      it 'should run the deploy:setup rake task' do
+        @rake.should.receive(:invoke)
+        run_command
+      end
+
+      it 'should make the specified target available as a "to" argument to the rake task' do
+        run_command
+        ENV['to'].should == 'foo'
+      end
+
+      it 'should fail if the rake task fails' do
+        @rake.stub!(:invoke).and_raise(RuntimeError)
+        lambda { run_command }.should.raise
+      end
+
+      it 'should not fail if the rake task succeeds' do
+        @rake.stub!(:invoke).and_return(true)
+        lambda { run_command }.should.not.raise
+      end
+    end
+  end
+  
+  describe "when the 'deploy' command is specified" do
+    describe 'but no target is specified' do
+      before do
+        Object.send(:remove_const, :ARGV)
+        ARGV = ['deploy']
+      end  
+
+      it 'should not run rake tasks' do
+        Rake::Application.should.receive(:new).never
+        lambda { run_command }
+      end
+  
+      it 'should fail' do
+        lambda { run_command }.should.raise
+      end
+    end
+    
+    describe 'and a --to argument is specified' do
+      before do
+        ARGV.push '--to=foo'
+        @rake = Rake::Task['deploy:now']
+        @rake.stub!(:invoke)
+      end
+      
+      it 'should not fail' do
+        lambda { run_command }.should.not.raise
+      end
+      
+      it 'should run the deploy:now rake task' do
+        @rake.should.receive(:invoke)
+        run_command
+      end
+      
+      it 'should make the specified target available as a "to" argument to the rake task' do
+        run_command
+        ENV['to'].should == 'foo'
+      end
+      
+      it 'should fail if the rake task fails' do
+        @rake.stub!(:invoke).and_raise(RuntimeError)
+        lambda { run_command }.should.raise
+      end
+      
+      it 'should not fail if the rake task succeeds' do
+        @rake.stub!(:invoke).and_return(true)
+        lambda { run_command }.should.not.raise
+      end
+    end
+
+    describe 'and a -t argument is specified' do
+      before do
+        ARGV.push '-t'
+        ARGV.push 'foo'
+        @rake = Rake::Task['deploy:now']
+        @rake.stub!(:invoke)
+      end
+
+      it 'should not fail' do
+        lambda { run_command }.should.not.raise
+      end
+
+      it 'should run the deploy:now rake task' do
+        @rake.should.receive(:invoke)
+        run_command
+      end
+
+      it 'should make the specified target available as a "to" argument to the rake task' do
+        run_command
+        ENV['to'].should == 'foo'
+      end
+
+      it 'should fail if the rake task fails' do
+        @rake.stub!(:invoke).and_raise(RuntimeError)
+        lambda { run_command }.should.raise
+      end
+
+      it 'should not fail if the rake task succeeds' do
+        @rake.stub!(:invoke).and_return(true)
+        lambda { run_command }.should.not.raise
+      end
+    end
+  end
+  
+  describe 'and more than one command is specified' do
+    before do
+      Object.send(:remove_const, :ARGV)
+      ARGV = ['frazzlebazzle', 'shizzlebizzle']
+    end
+    
+    it 'should fail if no target is specified' do
+      lambda { run_command }.should.raise
+    end
+    
+    it 'should fail even if a target is specified' do
+      ARGV.push('--to=foo')
+      lambda { run_command }.should.raise
+    end
+  end
+  
+  describe 'and an unknown command is specified' do
+    before do
+      Object.send(:remove_const, :ARGV)
+      ARGV = ['frazzlebazzle']
+    end
+    
+    it 'should fail when no target is specified' do
+      lambda { run_command }.should.raise
+    end
+    
+    it 'should fail when a target is specified' do
+      ARGV.push('--to=foo')
+      lambda { run_command }.should.raise
+    end
   end
 end
