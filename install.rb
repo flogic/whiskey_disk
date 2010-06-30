8f7f244e5131598f9d96b57bd2d89afa2391640c
Rick Bradley <rick@rickbradley.com>
2010-06-30 00:06:26 -0500

    make sure README.markdown is displayed on install

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 def readme_contents
-  IO.read(File.expand_path(File.join(File.dirname(__FILE__), 'README')))
+  IO.read(File.expand_path(File.join(File.dirname(__FILE__), 'README.markdown')))
 end
 
 puts readme_contents
