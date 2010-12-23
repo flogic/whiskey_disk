6d56ccde784d7b789a8b88d5ecaf286609b796ba
Rick Bradley <rick@rickbradley.com>
2010-12-22 22:57:52 -0500

    removing unnecessary describe blocks from config spec
    these actually obscure what we're doing here and will get in
    the way when we expand the examples that are valid for specifying
    domains

--- spec/whiskey_disk/config_spec.rb
+++ spec/whiskey_disk/config_spec.rb
@@ -226,50 +226,44 @@ describe WhiskeyDisk::Config do
         )
       end
     
-      describe 'and no domain has been specified' do
-        it 'should leave the domain as nil' do
-          WhiskeyDisk::Config.load_data['foo']['xyz']['domain'].should.be.nil     
-        end
-        
-        it 'should handle nil domains across all projects and targets' do
-          WhiskeyDisk::Config.load_data['zyx']['xyz']['domain'].should.be.nil     
-        end
+      it 'should leave the domain as nil when no domain is specified' do
+        WhiskeyDisk::Config.load_data['foo']['xyz']['domain'].should.be.nil     
+      end
+      
+      it 'should handle nil domains across all projects and targets' do
+        WhiskeyDisk::Config.load_data['zyx']['xyz']['domain'].should.be.nil     
       end
     
-      describe 'and a single domain has been specified' do
-        it 'should return domain as nil if the specified domain was empty' do
-          WhiskeyDisk::Config.load_data['foo']['eee']['domain'].should.be.nil       
-        end
-        
-        it 'should handle empty specified domains across all projects and targets' do
-          WhiskeyDisk::Config.load_data['zyx']['eee']['domain'].should.be.nil                 
-        end
+      it 'should return domain as nil if a single empty domain was specified' do
+        WhiskeyDisk::Config.load_data['foo']['eee']['domain'].should.be.nil       
+      end
       
-        it 'should return domain as a single list of the specified domain if the specified domain was not empty' do
-          WhiskeyDisk::Config.load_data['foo']['abc']['domain'].should == ['what@example.com']
-        end
-    
-        it 'should handle single specified domains across all projects and targets' do
-          WhiskeyDisk::Config.load_data['zyx']['abc']['domain'].should == ['what@example.com']
-        end
+      it 'should handle single empty specified domains across all projects and targets' do
+        WhiskeyDisk::Config.load_data['zyx']['eee']['domain'].should.be.nil                 
+      end
+      
+      it 'should return domain as a single element list if a single non-empty domain was specified' do
+        WhiskeyDisk::Config.load_data['foo']['abc']['domain'].should == ['what@example.com']
+      end
+  
+      it 'should handle single specified domains across all projects and targets' do
+        WhiskeyDisk::Config.load_data['zyx']['abc']['domain'].should == ['what@example.com']
       end
     
-      describe 'and a list of domains was specified' do
-        it 'should return the list of domains' do
-          WhiskeyDisk::Config.load_data['foo']['baz']['domain'].should == [ 'bar@example.com', 'baz@domain.com' ]
-        end
-      
-        it 'should handle lists of domains across all projects and targets' do
-          WhiskeyDisk::Config.load_data['zyx']['hij']['domain'].should == [ 'bar@example.com', 'baz@domain.com' ]
-        end
-      
-        it 'should remove any blank or nil domains from the list' do
-          WhiskeyDisk::Config.load_data['foo']['bar']['domain'].should == ['user@example.com', 'foo@domain.com' ]
-        end
+      it 'should return the list of domains when a list of domains is specified' do
+        WhiskeyDisk::Config.load_data['foo']['baz']['domain'].should == [ 'bar@example.com', 'baz@domain.com' ]
+      end
     
-        it 'should handle cleaning up blanks and nils across all projects and targets' do
-          WhiskeyDisk::Config.load_data['zyx']['def']['domain'].should == ['user@example.com', 'foo@domain.com' ]
-        end
+      it 'should handle lists of domains across all projects and targets' do
+        WhiskeyDisk::Config.load_data['zyx']['hij']['domain'].should == [ 'bar@example.com', 'baz@domain.com' ]
+      end
+      
+      it 'should remove any blank or nil domains from a simple list of domains' do
+        WhiskeyDisk::Config.load_data['foo']['bar']['domain'].should == ['user@example.com', 'foo@domain.com' ]
+      end
+  
+      it 'should handle cleaning up blanks and nils across all projects and targets' do
+        WhiskeyDisk::Config.load_data['zyx']['def']['domain'].should == ['user@example.com', 'foo@domain.com' ]
       end
     end
   end
