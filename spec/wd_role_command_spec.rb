998ac4230a4a07ac44dab7ff91e7798ff48ef124
Rick Bradley <rick@rickbradley.com>
2010-12-23 15:57:04 -0500

    new wd_role command for checking membership in a role from shell scripts
    Sets exit status appropriately.
    
    ( this example session presumes you `gem install rockhands` first... )
    
    $ export WD_ROLES='app:db'
    $ wd_role web && rock || shocker
         .-.
       .-.U|
       |U| | .-.
       | | |_|U|
       | | | | |
      /|     ` |
     | |       |
     |         |
      \        /
      |       |
      |       |
    
    $ wd_role app && rock || shocker
       .-.
       |U|
       | |   .-.
       | |-._|U|
       | | | | |
      /|     ` |
     | |       |
     |         |
              /
      |       |
      |       |

---
+++ spec/wd_role_command_spec.rb
@@ -0,0 +1,49 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+def run_command
+  eval File.read(File.join(File.dirname(__FILE__), *%w[.. bin wd_role]))
+end
+
+describe 'wd role command' do
+  before do
+    ENV['WD_ROLES'] = nil
+  end
+  
+  describe 'when no command-line arguments are specified' do
+    before do
+      Object.send(:remove_const, :ARGV)
+      ARGV = []
+    end  
+    
+    it 'should fail' do
+      lambda { run_command }.should.raise(SystemExit)
+    end
+  end
+  
+  describe "when a role is specified on the command-line" do
+    before do
+      Object.send(:remove_const, :ARGV)
+      ARGV = ['app']
+    end  
+
+    it 'should fail when no WD_ROLES environment setting is present' do
+      ENV['WD_ROLES'] = nil
+      lambda { run_command }.should.raise(SystemExit)
+    end
+    
+    it 'should fail when an empty WD_ROLES environment setting is present' do
+      ENV['WD_ROLES'] = ''
+      lambda { run_command }.should.raise(SystemExit)
+    end
+    
+    it 'should fail when the WD_ROLES environment setting does not contain that role' do
+      ENV['WD_ROLES'] = 'web:nonapp:db'
+      lambda { run_command }.should.raise(SystemExit)
+    end
+    
+    it 'should succeed when the WD_ROLES environment setting contains that role' do
+      ENV['WD_ROLES'] = 'web:app:db'
+      lambda { run_command }.should.not.raise
+    end    
+  end
+end
