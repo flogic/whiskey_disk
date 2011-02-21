f32d354fc545078b930edd76fa55c23ef0ee058e
Rick Bradley <rick@rickbradley.com>
2011-02-20 21:30:02 -0600

    basic integration specs for post_* scripts
    Note that 4 of these are failing.  The desired behavior when a specified
    post_* script is missing (i.e., fail the setup/deployment) is currently
    different from what is implemented.
    
    Will bring the behavior into line with what currently seems more appropriate.

---
+++ spec/integration/post_scripts_spec.rb
@@ -0,0 +1,216 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'whiskey_disk'))
+
+integration_spec do
+  describe 'when configured for a remote deployment' do
+    before do
+      setup_deployment_area
+    end
+  
+    describe 'with no post_* scripts defined' do
+      before do
+        @config = scenario_config('remote/deploy.yml')
+        @args = "--path=#{@config} --to=project:no_post_scripts"
+      end
+      
+      describe 'performing a setup' do
+        it 'should perform a checkout of the repository to the target path' do
+          run_setup(@args)
+          File.exists?(deployed_file('project/README')).should == true
+        end
+        
+        it 'should not run a post_setup script' do
+          run_setup(@args)
+          File.read(integration_log).should.not =~ /something something/ # TODO
+        end
+            
+        it 'should report the remote setup as successful' do
+          run_setup(@args)
+          File.read(integration_log).should =~ /wd-app1.example.com => succeeded/
+        end
+
+        it 'should exit with a true status' do
+          run_setup(@args).should == true
+        end
+      end
+      
+      describe 'performing a deployment' do
+        before do
+          checkout_repo('project')
+          File.unlink(deployed_file('project/README'))  # modify the deployed checkout
+        end
+        
+        it 'should update the checkout of the repository on the target path' do
+          run_deploy(@args)
+          File.exists?(deployed_file('project/README')).should == true
+        end    
+
+        it 'should not run a post_deploy script' do
+          run_deploy(@args)
+          File.read(integration_log).should.not =~ /something something/ # TODO
+        end
+            
+        it 'should report the remote deployment as successful' do
+          run_deploy(@args)
+          File.read(integration_log).should =~ /wd-app1.example.com => succeeded/
+        end
+
+        it 'should exit with a true status' do
+          run_deploy(@args).should == true
+        end
+      end
+    end
+    
+    describe 'with missing post_* scripts defined' do
+      before do
+        @config = scenario_config('remote/deploy.yml')
+        @args = "--path=#{@config} --to=project:missing_post_scripts"
+      end
+      
+      describe 'performing a setup' do
+        it 'should perform a checkout of the repository to the target path' do
+          run_setup(@args)
+          File.exists?(deployed_file('project/README')).should == true
+        end
+        
+        it 'should not run a post_setup script' do
+          run_setup(@args)
+          File.read(integration_log).should.not =~ /something something/ # TODO
+        end
+            
+        it 'should report the remote setup as a failure' do
+          run_setup(@args)
+          File.read(integration_log).should =~ /wd-app1.example.com => failed/
+        end
+
+        it 'should exit with a false status' do
+          run_setup(@args).should == false
+        end
+      end
+      
+      describe 'performing a deployment' do
+        before do
+          checkout_repo('project')
+          File.unlink(deployed_file('project/README'))  # modify the deployed checkout
+        end
+        
+        it 'should update the checkout of the repository on the target path' do
+          run_deploy(@args)
+          File.exists?(deployed_file('project/README')).should == true
+        end    
+
+        it 'should not run a post_deploy script' do
+          run_deploy(@args)
+          File.read(integration_log).should.not =~ /something something/ # TODO
+        end
+            
+        it 'should report the remote deployment as a failure' do
+          run_deploy(@args)
+          File.read(integration_log).should =~ /wd-app1.example.com => failed/
+        end
+
+        it 'should exit with a false status' do
+          run_deploy(@args).should == false
+        end
+      end      
+    end
+    
+    describe 'with post_* scripts specified and present' do
+      before do
+        @config = scenario_config('remote/deploy.yml')
+        @args = "--path=#{@config} --to=project:with_post_scripts"
+      end
+      
+      describe 'and doing a setup' do
+        it 'should perform a checkout of the repository to the target path' do
+          run_setup(@args)
+          File.exists?(deployed_file('project/README')).should == true
+        end
+        
+        describe 'and the post_setup script is successful' do
+          it 'should run the post_setup script' do
+            run_setup(@args)
+            File.read(integration_log).should =~ /^Running the post_setup script/
+          end
+
+          it 'should report the remote setup as successful' do
+            run_setup(@args)
+            File.read(integration_log).should =~ /wd-app1.example.com => succeeded/
+          end
+
+          it 'should exit with a true status' do
+            run_setup(@args).should == true
+          end          
+        end
+        
+        describe 'and the post_setup script fails' do
+          before do
+            @args = "--path=#{@config} --to=project:with_failing_post_scripts"
+          end
+          
+          it 'should run the post_setup script' do
+            run_setup(@args)
+            File.read(integration_log).should =~ /^Running the post_setup script/
+          end
+
+          it 'should report the remote setup as a failure' do
+            run_setup(@args)
+            File.read(integration_log).should =~ /wd-app1.example.com => failed/
+          end
+
+          it 'should exit with a false status' do
+            run_setup(@args).should == false
+          end          
+        end
+      end
+
+      describe 'and doing a deploy' do
+        before do
+          checkout_repo('project')
+          File.unlink(deployed_file('project/README'))  # modify the deployed checkout
+        end
+        
+        it 'should update the checkout of the repository on the target path' do
+          run_deploy(@args)
+          File.exists?(deployed_file('project/README')).should == true
+        end    
+
+        describe 'and the post_deploy script is successful' do
+          it 'should run the post_deploy script' do
+            run_deploy(@args)
+            File.read(integration_log).should =~ /^Running the post_deploy script/
+          end
+
+          it 'should report the remote deployment as successful' do
+            run_deploy(@args)
+            File.read(integration_log).should =~ /wd-app1.example.com => succeeded/
+          end
+
+          it 'should exit with a true status' do
+            run_deploy(@args).should == true
+          end          
+        end
+        
+        describe 'and the post_deploy script fails' do
+          before do
+            @args = "--path=#{@config} --to=project:with_failing_post_scripts"
+          end
+          
+          it 'should run the post_deploy script' do
+            run_deploy(@args)
+            File.read(integration_log).should =~ /^Running the post_deploy script/
+          end
+
+          it 'should report the remote deployment as a failure' do
+            run_deploy(@args)
+            File.read(integration_log).should =~ /wd-app1.example.com => failed/
+          end
+
+          it 'should exit with a false status' do
+            run_deploy(@args).should == false
+          end          
+        end
+      end
+    end
+  end
+end
