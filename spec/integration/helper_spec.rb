1bd6da0a6e1ed0fe13130722306fa7cdef26c285
Rick Bradley <rick@rickbradley.com>
2011-02-27 21:51:36 -0600

    adding #changed? helper (and friends) to whiskey_disk/helpers
    includes public helper methods for retrieving actual changes,
    reading raw changes files, getting paths to changes files, etc.
    
    There is an integration spec here, but it will fail at least one spec
    until the overall whiskey_disk deployment flow captures git and rsync
    changes successfully.

---
+++ spec/integration/helper_spec.rb
@@ -0,0 +1,43 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'whiskey_disk'))
+
+integration_spec do
+  describe 'when a post_deploy hook uses the changed? method' do
+    before do
+      setup_deployment_area
+      @config = scenario_config('remote/deploy.yml')
+      @args = "--path=#{@config} --to=project:hook_with_changed"
+    end
+    
+    describe 'and performing a deployment' do
+      before do
+        checkout_repo('project')
+        jump_to_initial_commit('project') # reset the deployed checkout
+      end
+
+      it 'should update the repo checkout' do
+        run_deploy(@args)
+        File.exists?(deployed_file('project/README')).should == true
+      end
+      
+      it 'should do actions guarded by a changed? method when the relevant files have changed' do
+        run_deploy(@args)
+        File.read(integration_log).should =~ /changed\? was true/
+      end
+
+      it 'should not do actions guarded by a changed? method when the relevant files have not changed' do
+        run_deploy(@args)
+        File.read(integration_log).should =~ /changed\? was false/
+      end
+
+      it 'should exit with a true status' do
+        run_deploy(@args).should == true
+      end      
+
+      it 'should report the deployment as successful' do
+        run_deploy(@args)
+        File.read(integration_log).should =~ /wd-app1.example.com => succeeded/        
+      end
+    end
+  end
+end
