3713b78d6bc59842c9c27efc7100455bbf5490fc
Rein Henrichs <rein@phpfog.com>
2011-03-11 21:29:04 -0800

    Only show rsync verbose and progress if debug enabled

--- lib/whiskey_disk.rb
+++ lib/whiskey_disk.rb
@@ -263,7 +263,7 @@ class WhiskeyDisk
     def refresh_configuration
       needs(:deploy_to, :deploy_config_to)
       raise "Must specify project name when using a configuration repository." unless project_name_specified?
-      enqueue("rsync -av --progress #{self[:deploy_config_to]}/#{self[:project]}/#{self[:config_target]}/ #{self[:deploy_to]}/")
+      enqueue("rsync -a#{'v --progress' if Config.debug?} #{self[:deploy_config_to]}/#{self[:project]}/#{self[:config_target]}/ #{self[:deploy_to]}/")
     end
     
     def run_post_setup_hooks
