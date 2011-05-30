41b52cb98cfbbf4067a5b04d06da616dcb377128
Rick Bradley <rick@rickbradley.com>
2011-05-30 17:48:11 -0500

    un-'should'-ifying remote deployment  specs

--- spec/integration/remote_deployments_spec.rb
+++ spec/integration/remote_deployments_spec.rb
@@ -14,28 +14,28 @@ integration_spec do
       end
 
       describe 'performing a setup' do
-        it 'should perform a checkout of the repository to the target path' do
+        it 'performs a checkout of the repository to the target path' do
           run_setup(@args)
           File.exists?(deployed_file('project/README')).should == true
         end
             
-        it 'should have the working copy set to the master branch' do
+        it 'has the working copy set to the master branch' do
           run_setup(@args)
           current_branch('project').should == 'master'
         end
 
-        it 'should have the working copy set to the specified branch when one is available' do
+        it 'has the working copy set to the specified branch when one is available' do
           @args = "--path=#{@config} --to=project:remote-on-other-branch"
           run_setup(@args)
           current_branch('project').should == 'no_rake_hooks'
         end
 
-        it 'should report the remote setup as successful' do
+        it 'reports the remote setup as successful' do
           run_setup(@args)
           File.read(integration_log).should =~ /wd-app1.example.com => succeeded/
         end
         
-        it 'should exit with a true status' do
+        it 'exits with a true status' do
           run_setup(@args).should == true
         end
       end
@@ -46,29 +46,29 @@ integration_spec do
           File.unlink(deployed_file('project/README'))  # modify the deployed checkout
         end
         
-        it 'should update the checkout of the repository on the target path' do
+        it 'updates the checkout of the repository on the target path' do
           run_deploy(@args)
           File.exists?(deployed_file('project/README')).should == true
         end    
 
-        it 'should have the working copy set to the master branch' do
+        it 'has the working copy set to the master branch' do
           run_deploy(@args)
           current_branch('project').should == 'master'
         end
 
-        it 'should have the working copy set to the specified branch when one is available' do
+        it 'has the working copy set to the specified branch when one is available' do
           @args = "--path=#{@config} --to=project:remote-on-other-branch"
           checkout_branch('project', 'no_rake_hooks')
           run_deploy(@args)
           current_branch('project').should == 'no_rake_hooks'          
         end
 
-        it 'should report the remote deployment as successful' do
+        it 'reports the remote deployment as successful' do
           run_deploy(@args)
           File.read(integration_log).should =~ /wd-app1.example.com => succeeded/
         end
 
-        it 'should exit with a true status' do
+        it 'exits with a true status' do
           run_deploy(@args).should == true
         end
       end
@@ -80,17 +80,17 @@ integration_spec do
             File.unlink(deployed_file('project/README'))  # modify the deployed checkout
           end
 
-          it 'should update the checkout of the repository on the target path' do
+          it 'updates the checkout of the repository on the target path' do
             run_deploy(@args)
             File.exists?(deployed_file('project/README')).should == true
           end    
 
-          it 'should have the working copy set to the master branch' do
+          it 'has the working copy set to the master branch' do
             run_deploy(@args)
             current_branch('project').should == 'master'
           end
 
-          it 'should have the working copy set to the specified branch when one is available' do
+          it 'has the working copy set to the specified branch when one is available' do
             setup_deployment_area
 
             @args = "--path=#{@config} --to=project:remote-on-other-branch"
@@ -102,12 +102,12 @@ integration_spec do
             current_branch('project').should == 'no_rake_hooks'          
           end
 
-          it 'should report the remote deployment as successful' do
+          it 'reports the remote deployment as successful' do
             run_deploy(@args)
             File.read(integration_log).should =~ /wd-app1.example.com => succeeded/
           end
 
-          it 'should exit with a true status' do
+          it 'exits with a true status' do
             run_deploy(@args).should == true
           end        
         end
