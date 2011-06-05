6b8506ef73bb952bcb2c866c6d50b6203bc349c1
Rick Bradley <rick@rickbradley.com>
2011-06-05 17:15:17 -0500

    add a wrapper spec for WD::Config#filter_data to protect us during refactoring

--- spec/whiskey_disk/config_spec.rb
+++ spec/whiskey_disk/config_spec.rb
@@ -610,6 +610,69 @@ describe WhiskeyDisk::Config do
     end
   end
 
+  describe 'filtering configuration data' do
+    before do
+      ENV['to'] = @env = 'foo:erl'
+      @data = {
+        'foo' => { 
+          'xyz' => { 'repository' => 'x' },
+          'eee' => { 'repository' => 'x', 'domain' => '' },
+          'abc' => { 'repository' => 'x', 'domain' => 'what@example.com' },
+          'baz' => { 'repository' => 'x', 'domain' => [ 'bar@example.com', 'baz@domain.com' ]},
+          'bar' => { 'repository' => 'x', 'domain' => [ 'user@example.com', nil, 'foo@domain.com' ]},
+          'bat' => { 'repository' => 'x', 'domain' => [ 'user@example.com', 'foo@domain.com', '' ]},
+          'hsh' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com' }, { 'name' => 'baz@domain.com' } ]},
+          'mix' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com' }, 'baz@domain.com' ]},            
+          'erl' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com', 'roles' => nil }, 
+                                                        { 'name' => 'baz@domain.com', 'roles' => '' },
+                                                        { 'name' => 'aok@domain.com', 'roles' => [] } ]},
+          'rol' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com', 'roles' => [ 'web', 'db' ] }, 
+                                                        { 'name' => 'baz@domain.com', 'roles' => [ 'db' ] },            
+                                                        { 'name' => 'aok@domain.com', 'roles' => 'app' } ]},            
+          'wow' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com', 'roles' => [ 'web', 'db' ] }, 
+                                                        { 'name' => 'baz@domain.com', 'roles' => [ 'db' ] },   
+                                                          '', 'foo@bar.example.com',      
+                                                        { 'name' => 'aok@domain.com', 'roles' => 'app' } ]},            
+        },
+  
+        'zyx' => {
+          'xyz' => { 'repository' => 'x' },
+          'eee' => { 'repository' => 'x', 'domain' => '' },
+          'abc' => { 'repository' => 'x', 'domain' => 'what@example.com' },
+          'hij' => { 'repository' => 'x', 'domain' => [ 'bar@example.com', 'baz@domain.com' ]},
+          'def' => { 'repository' => 'x', 'domain' => [ 'user@example.com', nil, 'foo@domain.com' ]},
+          'dex' => { 'repository' => 'x', 'domain' => [ 'user@example.com', 'foo@domain.com', '' ]},
+          'hsh' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com' }, { 'name' => 'baz@domain.com' } ]},
+          'mix' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com' }, 'baz@domain.com' ]},
+          'erl' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com', 'roles' => nil }, 
+                                                        { 'name' => 'baz@domain.com', 'roles' => '' },
+                                                        { 'name' => 'aok@domain.com', 'roles' => [] } ]},
+          'rol' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com', 'roles' => [ 'web', 'db' ] }, 
+                                                        { 'name' => 'baz@domain.com', 'roles' => [ 'db' ] },         
+                                                        { 'name' => 'aok@domain.com', 'roles' => 'app' } ]},            
+          'wow' => { 'repository' => 'x', 'domain' => [ { 'name' => 'bar@example.com', 'roles' => [ 'web', 'db' ] }, 
+                                                        { 'name' => 'baz@domain.com', 'roles' => [ 'db' ] },   
+                                                           '', 'foo@bar.example.com',      
+                                                        { 'name' => 'aok@domain.com', 'roles' => 'app' } ]},            
+        }
+      }
+    end
+    
+    it 'should apply all available filters' do
+      @config.filter_data(@data).should == {
+        "repository" => "x", 
+        "project" => "foo", 
+        "config_target" => "erl", 
+        "environment" => "erl",
+        "domain"     => [ 
+          { :name => "bar@example.com" }, 
+          { :name => "baz@domain.com" }, 
+          { :name => "aok@domain.com" }
+        ]
+      }
+    end
+  end
+
   describe 'computing the base path for the project' do
     before do
       @original_path = Dir.pwd
