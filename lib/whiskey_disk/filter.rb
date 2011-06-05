a4f84776708e19a5946e31462d238ca42e697806
Rick Bradley <rick@rickbradley.com>
2011-06-05 17:41:58 -0500

    refactoring to NormalizeDomainsFilter

--- lib/whiskey_disk/filter.rb
+++ lib/whiskey_disk/filter.rb
@@ -51,6 +51,51 @@ class WhiskeyDisk
       end
     end
     
+    class NormalizeDomainsFilter < AbstractFilter
+      def localize_domain_list(list)
+        [ list ].flatten.collect { |d| (d.nil? or d == '') ? 'local' : d }
+      end
+
+      def compact_list(list)
+        [ list ].flatten.delete_if { |d| d.nil? or d == '' }
+      end
+
+      # called only by normalize_domains
+      def normalize_domain(data)
+        compacted = localize_domain_list(data)
+        compacted = [ 'local' ] if compacted.empty?
+
+        compacted.collect do |d|
+          if d.respond_to?(:keys)
+            row = { :name => (d['name'] || d[:name]) }
+            roles = compact_list(d['roles'] || d[:roles])
+            row[:roles] = roles unless roles.empty?
+            row
+          else
+            { :name => d }
+          end
+        end
+      end
+
+      # called only by normalize_domains
+      def check_duplicates(project, target, domain_list)
+        seen = {}
+        domain_list.each do |domain|
+          raise "duplicate domain [#{domain[:name]}] in configuration file for project [#{project}], target [#{target}]" if seen[domain[:name]]
+          seen[domain[:name]] = true
+        end
+      end
+
+      def filter(data)
+        data.each_pair do |project, project_data|
+          project_data.each_pair do |target, target_data|
+            target_data['domain'] = check_duplicates(project, target, normalize_domain(target_data['domain']))
+          end
+        end
+        data
+      end
+    end
+    
     class Filter
       attr_reader :config
   
@@ -61,7 +106,7 @@ class WhiskeyDisk
       def filter_data(data)
         current = EnvironmentScopeFilter.new(config).filter(data.clone)
         current = ProjectScopeFilter.new(config).filter(current)
-        current = config.normalize_domains(current)
+        current = NormalizeDomainsFilter.new(config).filter(current)
         current = config.select_project_and_environment(current)
         current = config.add_environment_name(current)
         current = config.add_project_name(current)
