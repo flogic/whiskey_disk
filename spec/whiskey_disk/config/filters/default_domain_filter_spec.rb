04c4dd55906e96433ef5b7fd98fbb2b90c0d225d
Rick Bradley <rick@rickbradley.com>
2011-06-16 20:05:27 -0700

    filter to set a default domain when none is specified

---
+++ spec/whiskey_disk/config/filters/default_domain_filter_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'spec_helper.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'lib', 'whiskey_disk', 'config'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'lib', 'whiskey_disk', 'config', 'filters', 'default_domain_filter'))
+
+describe 'setting empty domain entries to "local"' do
+  before do
+    @config = WhiskeyDisk::Config.new
+    @filter = WhiskeyDisk::Config::DefaultDomainFilter.new(@config)
+  end
+  
+  it 'leaves data intact if it has a domain' do
+    @filter.filter({ 'domain' => 'anything' }).should == { 'domain' => 'anything' }
+  end
+
+  it 'adds a local domain entry if data does not have a domain' do
+    @filter.filter({}).should == { 'domain' => { 'name' => 'local' } }
+  end
+end
