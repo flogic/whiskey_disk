b83694105ac6a14c07de1feeabf49ebdb72f8554
Rick Bradley <rick@rickbradley.com>
2011-07-10 21:07:34 -0500

    automatically load filters and organize filter classes

--- lib/whiskey_disk/config/filter.rb
+++ lib/whiskey_disk/config/filter.rb
@@ -1,21 +1,4 @@
-require 'whiskey_disk/config/filters/stringify_hash_keys_filter'
-require 'whiskey_disk/config/filters/environment_scope_filter'
-require 'whiskey_disk/config/filters/project_scope_filter'
-require 'whiskey_disk/config/filters/select_project_and_environment_filter'
-require 'whiskey_disk/config/filters/check_repository_requirements_filter'
-require 'whiskey_disk/config/filters/check_config_repository_requirements_filter'
-require 'whiskey_disk/config/filters/add_environment_name_filter'
-require 'whiskey_disk/config/filters/add_project_name_filter'
-require 'whiskey_disk/config/filters/default_config_target_filter'
-require 'whiskey_disk/config/filters/default_domain_filter'
-require 'whiskey_disk/config/filters/hashify_domain_entries_filter'
-require 'whiskey_disk/config/filters/localize_domains_filter'
-require 'whiskey_disk/config/filters/check_for_duplicate_domains_filter'
-require 'whiskey_disk/config/filters/convert_role_strings_to_list_filter'
-require 'whiskey_disk/config/filters/drop_empty_domain_roles_filter'
-require 'whiskey_disk/config/filters/normalize_ssh_options_filter'
-require 'whiskey_disk/config/filters/set_default_branch_filter'
-require 'whiskey_disk/config/filters/set_default_config_branch_filter'
+Dir[File.join(File.dirname(__FILE__), 'filters/*_filter.rb')].each {|file| require file }
 
 class WhiskeyDisk
   class Config
@@ -26,23 +9,14 @@ class WhiskeyDisk
         @config = config
         @filters = [
           StringifyHashKeysFilter,
-          EnvironmentScopeFilter,
-          ProjectScopeFilter,
-          SelectProjectAndEnvironmentFilter,
-          CheckRepositoryRequirementsFilter,
-          CheckConfigRepositoryRequirementsFilter,
-          AddEnvironmentNameFilter,
-          AddProjectNameFilter,
-          DefaultConfigTargetFilter,
-          DefaultDomainFilter,
-          HashifyDomainEntriesFilter,
-          LocalizeDomainsFilter,
-          CheckForDuplicateDomainsFilter,
-          ConvertRoleStringsToListFilter,
-          DropEmptyDomainRolesFilter,
+          EnvironmentScopeFilter, ProjectScopeFilter, SelectProjectAndEnvironmentFilter,
+          CheckRepositoryRequirementsFilter, CheckConfigRepositoryRequirementsFilter,
+          AddEnvironmentNameFilter, AddProjectNameFilter,
+          DefaultConfigTargetFilter, DefaultDomainFilter,
+          HashifyDomainEntriesFilter, LocalizeDomainsFilter, CheckForDuplicateDomainsFilter,
+          ConvertRoleStringsToListFilter, DropEmptyDomainRolesFilter,
           NormalizeSshOptionsFilter,
-          SetDefaultBranchFilter,
-          SetDefaultConfigBranchFilter
+          SetDefaultBranchFilter, SetDefaultConfigBranchFilter
         ]
       end
   
