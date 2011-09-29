4b6da05937d15bf1b153b99c7afefd6602fd689f
Rick Bradley <rick@rickbradley.com>
2011-09-28 20:01:02 -0500

    make rsync_changes more robust
    Some versions of rsync report more flags -- let's be more accepting about what we
    recognize.

--- lib/whiskey_disk/helpers.rb
+++ lib/whiskey_disk/helpers.rb
@@ -22,7 +22,7 @@ end
 def rsync_changes
   changes = read_rsync_changes_file.split("\n")
   changes.map {|c| c.sub(/^[^ ]* [^ ]* [^ ]* /, '') }.
-          grep(/^[^ ]{9} /).map {|c| c.sub(/^[^ ]{9} /, '') }.
+          grep(/^[^ ]{9,} /).map {|c| c.sub(/^[^ ]{9,} /, '') }.
           map {|s| s.sub(%r{/$}, '') } - ['.']
 rescue Exception
   nil
